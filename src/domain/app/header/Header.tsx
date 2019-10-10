92361ee708a6a78fc2c651d4207c9c3864085748
Chi Nguyen <chi.nguyen@digia.com>
2019-10-10 14:34:15 +0300

    fix fail import

--- src/domain/app/header/Header.tsx
+++ src/domain/app/header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-import styles from './Header.module.scss';
+import styles from './header.module.scss';
 
 type Props = {};
 
