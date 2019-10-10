e3b73e5cbe48379a4d475edf54b70e0596416bc2
Bernt Andreas Drange <bernt.andreas.drange@digia.com>
2019-10-10 13:35:45 +0300

    Add Sentry to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 
 import './index.css';
 import BrowserApp from './domain/app/BrowserApp';
 import * as serviceWorker from './serviceWorker';
+if (process.env.REACT_APP_SENTRY_ENVIRONMENT) {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    environment: process.env.REACT_APP_SENTRY_ENVIRONMENT,
+    release: `${process.env.REACT_APP_APPLICATION_NAME}@${process.env.REACT_APP_VERSION}`,
+  });
+}
 
 ReactDOM.render(<BrowserApp />, document.getElementById('root'));
 
