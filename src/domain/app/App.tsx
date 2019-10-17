364f4624b45c8a978775da3e005183cd7f106e4e
Chi Nguyen <chi.nguyen@digia.com>
2019-10-17 12:57:12 +0300

    add locale and redirect to URL

--- src/domain/app/App.tsx
+++ src/domain/app/App.tsx
@@ -1,12 +1,23 @@
-import { Route, Switch } from 'react-router';
-import React from 'react';
+import { Route, Switch, RouteComponentProps, Redirect } from 'react-router';
+import React, { FunctionComponent } from 'react';
 
 import Home from '../home/Home';
+import NotFound from '../notFound/NotFound';
+import { changeLanguage } from '../../common/translation/utils';
 
-const App: React.FC = () => {
+const App: FunctionComponent<RouteComponentProps<{ locale: string }>> = ({
+  match: {
+    params: { locale },
+  },
+}) => {
+  changeLanguage(locale);
+
   return (
     <Switch>
-      <Route path="/" component={Home} />
+      <Redirect exact path={`/${locale}/`} to={`/${locale}/home`} />
+      <Route exact path={`/${locale}/home`} component={Home} />
+
+      <Route component={NotFound} />
     </Switch>
   );
 };
--- src/domain/app/BrowserApp.tsx
+++ src/domain/app/BrowserApp.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { I18nextProvider } from 'react-i18next';
+import { Switch, Route, Redirect } from 'react-router';
 
-import i18n from '../../common/translation/i18n/i18nInit';
 import App from './App';
+import { SUPPORT_LANGUAGES } from '../../common/translation/constants';
 
-function BrowserApp() {
-  return (
-    <BrowserRouter>
-      <I18nextProvider i18n={i18n}>
-        <App />
-      </I18nextProvider>
-    </BrowserRouter>
-  );
-}
+const localeParam = `:locale(${SUPPORT_LANGUAGES.EN}|${SUPPORT_LANGUAGES.FI}|${SUPPORT_LANGUAGES.SV})`;
+
+export const appRoutes = (
+  <Switch>
+    <Redirect exact path="/" to="/fi/home" />
+    <Route path={`/${localeParam}/*`} component={App} />
+    <Route
+      render={props => <Redirect to={`/fi${props.location.pathname}`} />}
+    />
+  </Switch>
+);
+// Export for testing purpose
+
+const BrowserApp: FunctionComponent = () => {
+  return <BrowserRouter>{appRoutes}</BrowserRouter>;
+};
 
 export default BrowserApp;
