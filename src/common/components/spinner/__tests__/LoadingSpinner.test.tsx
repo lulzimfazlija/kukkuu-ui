334207d3e43aa4ccd862b1da63dbfcc839270b55
Chi Nguyen <chi.nguyen@digia.com>
2019-10-29 13:50:45 +0200

    add unit test for spinner

--- src/common/components/spinner/__tests__/LoadingSpinner.test.tsx
+++ src/common/components/spinner/__tests__/LoadingSpinner.test.tsx
@@ -7,3 +7,17 @@ it('renders snapshot correctly', () => {
   const spinner = shallow(<LoadingSpinner isLoading={false} />);
   expect(spinner.html()).toMatchSnapshot();
 });
+
+it('render spinner if isLoading is true', () => {
+  const spinner = shallow(<LoadingSpinner isLoading={true} />).children();
+  expect(spinner.prop('className')).toContain('spinner');
+});
+
+it('render child component if isLoading is false', () => {
+  const spinner = shallow(
+    <LoadingSpinner isLoading={false}>
+      <div className="component"></div>
+    </LoadingSpinner>
+  ).children();
+  expect(spinner.prop('className')).toEqual('component');
+});
