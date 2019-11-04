8e95c1142ce98c2f14c1c94d920c7b3cacfa8047
Chi Nguyen <chi.nguyen@digia.com>
2019-11-04 14:29:43 +0200

    rename all birthDay to birthday

--- src/common/components/form/__tests__/validationUtils.tests.tsx
+++ src/common/components/form/__tests__/validationUtils.tests.tsx
@@ -1,29 +1,29 @@
 import {
-  validateBirthDay,
+  validateBirthday,
   validateEqual,
   validateRequire,
 } from '../validationUtils';
 
 describe('Form validation utilities - ', () => {
-  describe('validateBirthDay', () => {
+  describe('validateBirthday', () => {
     test('return require text if value is empty', () => {
-      const error = validateBirthDay('');
+      const error = validateBirthday('');
 
       expect(error).toEqual('Required');
     });
 
     test('return wrong format error if user try to input number, or general text', () => {
-      const error = validateBirthDay('foo');
+      const error = validateBirthday('foo');
       expect(error).toEqual('Invalid date input');
     });
 
     test('return wrong format error if user try to wrong format date', () => {
-      const error = validateBirthDay('1f10.149.13');
+      const error = validateBirthday('1f10.149.13');
       expect(error).toEqual('Invalid date input');
     });
 
     test('return wrong unsupported message if kid birthday is in future or before supported year', () => {
-      const error = validateBirthDay('12.12.2100');
+      const error = validateBirthday('12.12.2100');
       expect(error).toEqual('Input date is not supported');
     });
   });
