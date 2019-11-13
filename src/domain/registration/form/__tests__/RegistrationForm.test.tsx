29fbc9db7963e5e6af7c28f8753bd942d5bc8ece
Bernt Andreas Drange <bernt.andreas.drange@digia.com>
2019-11-13 14:23:04 +0200

    Fix tests after adding a mutation hook invocation

--- src/domain/registration/form/__tests__/RegistrationForm.test.tsx
+++ src/domain/registration/form/__tests__/RegistrationForm.test.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { MockedProvider } from '@apollo/react-testing';
 
 import { UnconnectedRegistrationForm } from '../RegistrationForm';
 import { defaultRegistrationData } from '../../state/RegistrationReducers';
 
 it('renders snapshot correctly', () => {
   const element = shallow(
-    <UnconnectedRegistrationForm
-      setFormValues={jest.fn()}
-      initialValues={defaultRegistrationData.formValues}
-    />
+    <MockedProvider>
+      <UnconnectedRegistrationForm
+        setFormValues={jest.fn()}
+        initialValues={defaultRegistrationData.formValues}
+      />
+    </MockedProvider>
   );
   expect(element.html()).toMatchSnapshot();
 });
