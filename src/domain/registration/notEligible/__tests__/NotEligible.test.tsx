e3ea380e03abe91b4fc51deb3d858107f7416327
Bernt Andreas Drange <bernt.andreas.drange@digia.com>
2019-11-20 13:27:06 +0200

    Try to kick Travis into passing tests

--- src/domain/registration/notEligible/__tests__/NotEligible.test.tsx
+++ src/domain/registration/notEligible/__tests__/NotEligible.test.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
 
 import NotEligible from '../NotEligible';
 
 it('renders snapshot correctly', () => {
   const element = shallow(<NotEligible />);
-  expect(toJson(element)).toMatchSnapshot();
+  expect(element.html()).toMatchSnapshot();
 });
