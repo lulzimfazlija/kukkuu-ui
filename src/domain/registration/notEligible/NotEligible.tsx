c952c2a0ee490389fc3f22dc4835ea083655c3d7
Chi Nguyen <chi.nguyen@digia.com>
2019-11-22 11:34:05 +0200

    fix snapshot tests

--- src/domain/registration/notEligible/NotEligible.tsx
+++ src/domain/registration/notEligible/NotEligible.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useHistory } from 'react-router';
+import { useHistory } from 'react-router-dom';
 
 import styles from './notEligible.module.scss';
 import Button from '../../../common/components/button/Button';
