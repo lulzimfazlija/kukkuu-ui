4b5f8753c47810c21f5aeab6aa9b4740a9a50c92
Chi Nguyen <chi.nguyen@digia.com>
2019-11-25 16:02:55 +0200

    add notice for future

--- src/domain/child/types/ChildTypes.ts
+++ src/domain/child/types/ChildTypes.ts
@@ -2,6 +2,7 @@ import { ChildInput } from '../../api/generatedTypes/globalTypes';
 
 export interface Child extends ChildInput {
   postalCode?: string;
+  // TODO: Remove me when backend actually support homeCity to single child
   homeCity: string;
 }
 
