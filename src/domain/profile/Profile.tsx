fae1f9b1cc655f7c46e3eadd6c42ebe163cdf109
Bernt Andreas Drange <bernt.andreas.drange@digia.com>
2019-11-27 12:01:55 +0200

    Send user to home page after logging in if they don't have data in api

--- src/domain/profile/Profile.tsx
+++ src/domain/profile/Profile.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import { connect } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
 
@@ -40,48 +41,57 @@ interface Props {
 const Profile: FunctionComponent<Props> = ({ profileToStore }) => {
   const { loading, error, data } = useQuery(profileQuery);
   const { t } = useTranslation();
+  const history = useHistory();
 
   if (loading) return <div>'Loading...'</div>;
   if (error) return <div>Error! {error.message}</div>;
 
-  const guardian = {
-    phone: data.guardians.edges[0].node.phone,
-    firstName: data.guardians.edges[0].node.firstName,
-    lastName: data.guardians.edges[0].node.lastName,
-  };
+  /*
+   * If the guardian does not exist in the api, they have not registered and we want to send them to the front page.
+   */
+  if (data.guardians.edges.length === 0) {
+    history.push('/home');
+    return <div>No profile exists</div>;
+  } else {
+    const guardian = {
+      phone: data.guardians.edges[0].node.phone,
+      firstName: data.guardians.edges[0].node.firstName,
+      lastName: data.guardians.edges[0].node.lastName,
+    };
 
-  const children: Children = normalizeChildren(
-    data.guardians.edges[0].node.children.edges
-  );
+    const children: Children = normalizeChildren(
+      data.guardians.edges[0].node.children.edges
+    );
 
-  const profileValues: GuardianValues = {
-    firstName: guardian.firstName,
-    lastName: guardian.lastName,
-    phoneNumber: guardian.phone,
-    children,
-  };
+    const profileValues: GuardianValues = {
+      firstName: guardian.firstName,
+      lastName: guardian.lastName,
+      phoneNumber: guardian.phone,
+      children,
+    };
 
-  const payload = Object.assign({}, profileValues, {});
-  profileToStore(payload);
+    const payload = Object.assign({}, profileValues, {});
+    profileToStore(payload);
 
-  return (
-    <div>
-      <h1>{t('profile.heading')}</h1>
+    return (
       <div>
-        {children.map(child => (
-          <div>
-            {child.firstName} {child.lastName} {child.birthdate}
-          </div>
-        ))}
+        <h1>{t('profile.heading')}</h1>
+        <div>
+          {children.map(child => (
+            <div>
+              {child.firstName} {child.lastName} {child.birthdate}
+            </div>
+          ))}
+        </div>
+        <div>TODO: email</div>
+        <div>
+          {guardian.firstName} {guardian.lastName}
+        </div>
+        <div>{guardian.phone}</div>
+        <NoUpcomingEvents />
       </div>
-      <div>TODO: email</div>
-      <div>
-        {guardian.firstName} {guardian.lastName}
-      </div>
-      <div>{guardian.phone}</div>
-      <NoUpcomingEvents />
-    </div>
-  );
+    );
+  }
 };
 
 const actions = {
