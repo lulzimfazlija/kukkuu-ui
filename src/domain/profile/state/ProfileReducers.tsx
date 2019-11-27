d3c68db1fb224d148ecd7251e20f934a6c58b913
Bernt Andreas Drange <bernt.andreas.drange@digia.com>
2019-11-27 12:43:36 +0200

    Add TODO about profile data structure improvement

--- src/domain/profile/state/ProfileReducers.tsx
+++ src/domain/profile/state/ProfileReducers.tsx
@@ -3,6 +3,7 @@ import { createReducer } from 'redux-starter-kit';
 import { ProfileData } from '../types/ProfileTypes';
 import { PROFILE_ACTIONS } from '../constants/ProfileActionConstants';
 
+// TODO: Improve the structure of the data.
 export const defaultProfileValules: ProfileData = {
   guardian: {
     firstName: '',
