0ddaee02b4168526572494efed827233cd0e7482
Chi Nguyen <chi.nguyen@digia.com>
2019-12-09 13:08:47 +0200

    use /profile support from backend

--- src/domain/profile/type/ProfileTypes.ts
+++ src/domain/profile/type/ProfileTypes.ts
@@ -1,6 +1,6 @@
 import {
-  profileQuery_guardians_edges_node as GeneratedProfile,
-  profileQuery_guardians_edges_node_children_edges_node as GeneratedProfileChild,
+  profileQuery_myProfile as GeneratedProfile,
+  profileQuery_myProfile_children_edges_node as GeneratedProfileChild,
 } from '../../api/generatedTypes/profileQuery';
 
 export interface ProfileType extends Omit<GeneratedProfile, 'children'> {
