0ecc6fd4c8daa7057d383d3d2ed88ab55e96b680
Chi Nguyen <chi.nguyen@digia.com>
2019-12-11 13:45:41 +0200

    add hero layout

--- src/domain/home/Home.tsx
+++ src/domain/home/Home.tsx
@@ -1,25 +1,17 @@
 import React, { FunctionComponent } from 'react';
-import { useTranslation } from 'react-i18next';
 
 import styles from './home.module.scss';
 import HomePreliminaryForm from './form/HomePreliminaryForm';
 import PageWrapper from '../app/layout/PageWrapper';
+import HomeHero from './hero/HomeHero';
 
 const Home: FunctionComponent = () => {
-  const { t } = useTranslation();
-
   return (
     <PageWrapper>
       <div className={styles.home}>
-        <div className={styles.hero}>
-          <div className={styles.heroContainer}>
-            <h1>{t('homePage.hero.heading')}</h1>
-            <p className={styles.description}>
-              {t('homePage.hero.descriptionText')}
-            </p>
-            <HomePreliminaryForm />
-          </div>
-        </div>
+        <HomeHero userHasProfile={false} />
+
+        <HomePreliminaryForm />
       </div>
     </PageWrapper>
   );
