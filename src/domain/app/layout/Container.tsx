6d33f7d2b20cb6bc18451825869cfa10d9daa7c1
Kimmo Virtanen <kimmo.virtanen@phz.fi>
2019-12-20 16:39:42 +0200

    we need to be able to distinguish home container from other containers.

--- src/domain/app/layout/Container.tsx
+++ src/domain/app/layout/Container.tsx
@@ -2,13 +2,23 @@ import React, { FunctionComponent } from 'react';
 import classnames from 'classnames';
 
 import styles from './container.module.scss';
-const Container: FunctionComponent<{ className?: string }> = ({
+const Container: FunctionComponent<{
+  children?: any
+  className?: string
+}> = ({
   children,
   className,
 }) => {
+  const isHome = (children && children.props.className === 'home_home__kC8RU') ? true : false;
   return (
-    <div className={classnames(styles.container, className)}>{children}</div>
-  );
+      <div>
+        { isHome ? (
+            <div className={classnames(styles.container, className, styles.ishome)}>{children}</div>
+        ) : (
+            <div className={classnames(styles.container, className)}>{children}</div>
+        )}
+      </div>
+  )
 };
 
 export default Container;
