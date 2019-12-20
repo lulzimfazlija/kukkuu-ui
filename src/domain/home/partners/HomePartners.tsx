6bd22e733319888def23742c24cd41e15a44f231
Bernt Andreas Drange <bernt.andreas.drange@digia.com>
2019-12-20 13:30:05 +0200

    Rename partner icons to be consistent

--- src/domain/home/partners/HomePartners.tsx
+++ src/domain/home/partners/HomePartners.tsx
@@ -3,11 +3,11 @@ import { useTranslation } from 'react-i18next';
 
 import styles from './homePartners.module.scss';
 import cirkoIcon from '../../../assets/icons/partners/cirko.png';
-import hekaIcon from '../../../assets/icons/partners/heka.png';
-import hkiIcon from '../../../assets/icons/partners/hki.png';
-import ilmiIcon from '../../../assets/icons/partners/ilmi.png';
-import janeIcon from '../../../assets/icons/partners/jane.png';
-import tansiIcon from '../../../assets/icons/partners/tansi.png';
+import helsinginkaupunginorkesteriIcon from '../../../assets/icons/partners/helsinginkaupunginorkesteri.png';
+import helIcon from '../../../assets/icons/partners/hel.png';
+import teatteriilmioIcon from '../../../assets/icons/partners/teatteriilmio.png';
+import jaesIcon from '../../../assets/icons/partners/jaes.png';
+import tanssintaloIcon from '../../../assets/icons/partners/tanssintalo.png';
 import Icon from '../../../common/components/icon/Icon';
 
 const HomePartners: React.FunctionComponent = props => {
@@ -18,14 +18,18 @@ const HomePartners: React.FunctionComponent = props => {
       <h2>{t('home.partners.heading.text')}</h2>
       <div className={styles.partners}>
         <div className={styles.big}>
-          <Icon className={styles.icon} src={janeIcon} alt="Jane icon" />
-          <Icon className={styles.icon} src={hkiIcon} alt="Hki icon" />
+          <Icon className={styles.icon} src={jaesIcon} alt="" />
+          <Icon className={styles.icon} src={helIcon} alt="" />
         </div>
         <div className={styles.small}>
-          <Icon className={styles.icon} src={hekaIcon} alt="Heka icon" />
-          <Icon className={styles.icon} src={ilmiIcon} alt="Ilmi icon" />
+          <Icon
+            className={styles.icon}
+            src={helsinginkaupunginorkesteriIcon}
+            alt=""
+          />
+          <Icon className={styles.icon} src={teatteriilmioIcon} alt="" />
           <Icon className={styles.icon} src={cirkoIcon} alt="Cirko icon" />
-          <Icon className={styles.icon} src={tansiIcon} alt="Tansi icon" />
+          <Icon className={styles.icon} src={tanssintaloIcon} alt="" />
         </div>
       </div>
     </section>
