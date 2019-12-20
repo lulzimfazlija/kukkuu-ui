f93307354f519cb44970815173cd57c2ad6e5ea9
Bernt Andreas Drange <bernt.andreas.drange@digia.com>
2019-12-20 17:11:13 +0200

    Refactor HomePartners component

--- src/domain/home/partners/HomePartners.tsx
+++ src/domain/home/partners/HomePartners.tsx
@@ -29,86 +29,81 @@ import teatteriilmioIcon from '../../../assets/icons/partners/teatteriilmio.png'
 import teatterimuseoIcon from '../../../assets/icons/partners/teatterimuseo.png';
 import valokuvataiteenmuseoIcon from '../../../assets/icons/partners/valokuvataiteenmuseo.png';
 
-const HomePartners: React.FunctionComponent = props => {
+interface Partner {
+  name: string;
+  icon: string;
+}
+
+type Props = {
+  className: string;
+  partners: Partner[];
+};
+
+// Name, icon file name and translation key is created from partner's domain
+// name without .fi and dash.
+// Examples:
+// teatteri-ilmio.fi -> teatteriilmio
+// hel.fi -> hel
+const mainPartnerList: Partner[] = [
+  { name: 'hel', icon: helIcon },
+  { name: 'jaes', icon: jaesIcon },
+];
+const partnerList: Partner[] = [
+  { name: 'amosrex', icon: amosrexIcon },
+  { name: 'cirko', icon: cirkoIcon },
+  { name: 'designmuseum', icon: designmuseumIcon },
+  { name: 'hamhelsinki', icon: hamhelsinkiIcon },
+  { name: 'helsinginkaupunginmuseo', icon: helsinginkaupunginmuseoIcon },
+  {
+    name: 'helsinginkaupunginorkesteri',
+    icon: helsinginkaupunginorkesteriIcon,
+  },
+  { name: 'hkt', icon: hktIcon },
+  { name: 'hotellijaravintolamuseo', icon: hotellijaravintolamuseoIcon },
+  { name: 'hurjaruuth', icon: hurjaruuthIcon },
+  { name: 'kansallismuseo', icon: kansallismuseoIcon },
+  { name: 'kansallisteatteri', icon: kansallisteatteriIcon },
+  { name: 'kavi', icon: kaviIcon },
+  { name: 'kulttuuriperintokasvatus', icon: kulttuuriperintokasvatusIcon },
+  { name: 'mfa', icon: mfaIcon },
+  { name: 'nukketeatterisampo', icon: nukketeatterisampoIcon },
+  { name: 'oodihelsinki', icon: oodihelsinkiIcon },
+  { name: 'oopperabaletti', icon: oopperabalettiIcon },
+  { name: 'qteatteri', icon: qteatteriIcon },
+  { name: 'svenskateatern', icon: svenskateaternIcon },
+  { name: 'tanssintalo', icon: tanssintaloIcon },
+  { name: 'teatteriilmio', icon: teatteriilmioIcon },
+  { name: 'teatterimuseo', icon: teatterimuseoIcon },
+  { name: 'valokuvataiteenmuseo', icon: valokuvataiteenmuseoIcon },
+];
+
+const Partners: React.FunctionComponent<Props> = props => {
   const { t } = useTranslation();
 
-  interface Partner {
-    name: string;
-    icon: string;
-  }
+  const partners = props.partners;
+  return (
+    <div className={props.className}>
+      {partners.map((partner: Partner, index: number) => (
+        <Icon
+          key={index}
+          className={styles.icon}
+          src={partner.icon}
+          alt={t(`home.partners.partner.${partner.name}`)}
+        />
+      ))}
+    </div>
+  );
+};
 
-  type Props = {
-    partners: Partner[];
-  };
-
-  // Name, icon file name and translation key is created from partner's domain
-  // name without .fi and dash.
-  // Examples:
-  // teatteri-ilmio.fi -> teatteriilmio
-  // hel.fi -> hel
-  const mainPartnerList: Partner[] = [
-    { name: 'hel', icon: helIcon },
-    { name: 'jaes', icon: jaesIcon },
-  ];
-  const partnerList: Partner[] = [
-    { name: 'amosrex', icon: amosrexIcon },
-    { name: 'cirko', icon: cirkoIcon },
-    { name: 'designmuseum', icon: designmuseumIcon },
-    { name: 'hamhelsinki', icon: hamhelsinkiIcon },
-    { name: 'helsinginkaupunginmuseo', icon: helsinginkaupunginmuseoIcon },
-    {
-      name: 'helsinginkaupunginorkesteri',
-      icon: helsinginkaupunginorkesteriIcon,
-    },
-    { name: 'hkt', icon: hktIcon },
-    { name: 'hotellijaravintolamuseo', icon: hotellijaravintolamuseoIcon },
-    { name: 'hurjaruuth', icon: hurjaruuthIcon },
-    { name: 'kansallismuseo', icon: kansallismuseoIcon },
-    { name: 'kansallisteatteri', icon: kansallisteatteriIcon },
-    { name: 'kavi', icon: kaviIcon },
-    { name: 'kulttuuriperintokasvatus', icon: kulttuuriperintokasvatusIcon },
-    { name: 'mfa', icon: mfaIcon },
-    { name: 'nukketeatterisampo', icon: nukketeatterisampoIcon },
-    { name: 'oodihelsinki', icon: oodihelsinkiIcon },
-    { name: 'oopperabaletti', icon: oopperabalettiIcon },
-    { name: 'qteatteri', icon: qteatteriIcon },
-    { name: 'svenskateatern', icon: svenskateaternIcon },
-    { name: 'tanssintalo', icon: tanssintaloIcon },
-    { name: 'teatteriilmio', icon: teatteriilmioIcon },
-    { name: 'teatterimuseo', icon: teatterimuseoIcon },
-    { name: 'valokuvataiteenmuseo', icon: valokuvataiteenmuseoIcon },
-  ];
-
-  function Partners(props: Props) {
-    // any is workaround for TS bug
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const partners: any = props.partners;
-    const partnerItems = partners.map((partner: Partner, index: number) => (
-      <Icon
-        key={index}
-        className={styles.icon}
-        src={partner.icon}
-        alt={t(`home.partners.partner.${partner.name}`)}
-      />
-    ));
-    return partnerItems;
-  }
+const HomePartners: React.FunctionComponent = props => {
+  const { t } = useTranslation();
 
   return (
     <section className={styles.wrapper}>
       <h2>{t('home.partners.heading.text')}</h2>
       <div className={styles.partners}>
-        <div className={styles.big}>
-          <Partners partners={mainPartnerList} />
-        </div>
-        <div className={styles.small}>
-          <Partners partners={partnerList} />
-          <Icon
-            className={styles.icon}
-            src={helsinginkaupunginorkesteriIcon}
-            alt=""
-          />
-        </div>
+        <Partners className={styles.big} partners={mainPartnerList} />
+        <Partners className={styles.small} partners={partnerList} />
       </div>
     </section>
   );
