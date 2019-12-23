c06dd6aea58d72e3ced931d8f217795fa2eccc3c
Kimmo Virtanen <kimmo.virtanen@phz.fi>
2019-12-23 11:47:52 +0200

    If class name starts with home_home_ it is the home content.

--- src/domain/app/layout/Container.tsx
+++ src/domain/app/layout/Container.tsx
@@ -3,6 +3,10 @@ import classnames from 'classnames';
 
 import styles from './container.module.scss';
 
+const stringStartsWith = (targetString: string, searchString: string) => {
+  return targetString.substring(0, searchString.length) === searchString;
+}
+
 const Container: FunctionComponent<{
   children?: any
   className?: string
@@ -10,7 +14,7 @@ const Container: FunctionComponent<{
   children,
   className,
 }) => {
-  const isHome = (children && children.props.className === 'home_home__kC8RU') ? true : false;
+  const isHome = (children.props.className && stringStartsWith(children.props.className, 'home_home_'));
   return (
       <div>
         { isHome ? (
