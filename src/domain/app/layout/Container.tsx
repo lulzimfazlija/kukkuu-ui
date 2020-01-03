8f106cc848df23177c549dd1c056c5b3dd797f92
Kimmo Virtanen <kimmo.virtanen@phz.fi>
2020-01-03 09:44:43 +0200

    Refactoring. Now gridLayoutOverride is passed from Home to PageWrapper to Container.

--- src/domain/app/layout/Container.tsx
+++ src/domain/app/layout/Container.tsx
@@ -3,35 +3,11 @@ import classnames from 'classnames';
 
 import styles from './container.module.scss';
 
-const stringStartsWith = (targetString: string, searchString: string) => {
-  return targetString.substring(0, searchString.length) === searchString;
-};
-
 const Container: FunctionComponent<{
-  children?: any;
   className?: string;
 }> = ({ children, className }) => {
-  const isHome =
-    children.props.className &&
-    stringStartsWith(children.props.className, 'home_home_');
   return (
-    <div>
-      {isHome ? (
-        <div
-          className={classnames(
-            styles.container,
-            className,
-            styles.gridLayoutOverride
-          )}
-        >
-          {children}
-        </div>
-      ) : (
-        <div className={classnames(styles.container, className)}>
-          {children}
-        </div>
-      )}
-    </div>
+    <div className={classnames(styles.container, className)}>{children}</div>
   );
 };
 
