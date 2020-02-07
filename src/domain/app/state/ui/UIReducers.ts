8c69ddc31fbe7bc5c5eb50341d2243c7b8e44a91
Chi Nguyen <chi.nguyen@digia.com>
2020-02-07 11:27:33 +0200

    fix reset state doesnt applied

--- src/domain/app/state/ui/UIReducers.ts
+++ src/domain/app/state/ui/UIReducers.ts
@@ -16,7 +16,5 @@ export default createReducer(defaultUIState, {
   [UI_ACTIONS.SESSION_EXPIRED_PROMPT.CLOSE]: (state, action) => {
     state.sessionExpiredPrompt.isOpen = false;
   },
-  [UI_ACTIONS.RESET_UI_STATE]: state => {
-    state = defaultUIState;
-  },
+  [UI_ACTIONS.RESET_UI_STATE]: state => (state = defaultUIState),
 });
