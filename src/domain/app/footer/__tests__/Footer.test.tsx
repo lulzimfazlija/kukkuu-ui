87bfd85466a6046ea00495f31b8b567772a75a0f
Bernt Andreas Drange <bernt.andreas.drange@digia.com>
2020-02-12 18:05:17 +0200

    Fix tests

--- src/domain/app/footer/__tests__/Footer.test.tsx
+++ src/domain/app/footer/__tests__/Footer.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Footer from '../Footer';
 
 it('renders snapshot correctly', () => {
-  const footer = shallow(<Footer />);
+  const footer = shallow(
+    <Router>
+      <Footer />
+    </Router>
+  );
   expect(footer.html()).toMatchSnapshot();
 });
