1028077ad02977350cb542451f72b343151ea1da
Bernt Andreas Drange <bernt.andreas.drange@digia.com>
2020-02-20 14:39:28 +0200

    Show invitation label on child when enrollment is possible

--- src/domain/api/generatedTypes/globalTypes.ts
+++ src/domain/api/generatedTypes/globalTypes.ts
@@ -1,10 +1,20 @@
 /* tslint:disable */
 /* eslint-disable */
+// @generated
 // This file was automatically generated and should not be edited.
 
 //==============================================================
 // START Enums and Input Objects
 //==============================================================
+
+/**
+ * An enumeration.
+ */
+export enum EventTranslationLanguageCode {
+  EN = "EN",
+  FI = "FI",
+  SV = "SV",
+}
 
 /**
  * An enumeration.
