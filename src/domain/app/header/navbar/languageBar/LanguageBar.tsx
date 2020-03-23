10fa16b7f76733a31a5cb414213b0267f0b4a854
Bernt Andreas Drange <bernt.andreas.drange@digia.com>
2020-03-23 13:06:40 +0200

    Reload page when changing language also on desktop
    The mobile implementation already works like that

--- src/domain/app/header/navbar/languageBar/LanguageBar.tsx
+++ src/domain/app/header/navbar/languageBar/LanguageBar.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { useLocation } from 'react-router';
 import { useTranslation } from 'react-i18next';
-import { NavLink } from 'react-router-dom';
 import classnames from 'classnames';
 
 import styles from './languageBar.module.scss';
@@ -19,18 +18,16 @@ const LanguageBar: React.FunctionComponent<{ className?: string }> = ({
   return (
     <div className={classnames(className, styles.languageBar)}>
       {Object.values(SUPPORT_LANGUAGES).map((language, index) => (
-        <NavLink
+        <a
           key={index}
-          activeClassName={styles.active}
-          onClick={() => i18n.changeLanguage(language)}
-          isActive={(match, location) =>
-            location.pathname.includes(`/${language}/`)
+          aria-current={
+            location.pathname.includes(`/${language}/`) ? 'page' : 'false'
           }
-          to={updateLocaleParam(location.pathname, currentLanguage, language)}
+          href={updateLocaleParam(location.pathname, currentLanguage, language)}
           lang={language}
         >
           {t(`common.language.${language}`)}
-        </NavLink>
+        </a>
       ))}
     </div>
   );
